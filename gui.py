a7fec02c38b5e247bbfd6958834829f76eb37869
Julien Pontabry <julien.pontabry@gmail.com>
2019-07-21 12:24:28 +0200

    Arrange building of the frame and test central widget.

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ This module contains everything related to the GUI.
 
 import sys
 
-from PySide2.QtWidgets import QMainWindow, QVBoxLayout, QLabel, QAction, QWidget, QPushButton
+from PySide2.QtWidgets import QMainWindow, QAction, QWidget, QMenuBar, QStatusBar, QTableWidget, QTableWidgetItem
 
 
 class DataGingerWindow(QMainWindow):
@@ -13,47 +13,93 @@ class DataGingerWindow(QMainWindow):
     """
 
     def __init__(self, *args, **kwargs):
-        super(DataGingerWindow, self).__init__(*args, **kwargs)
-        self.setWindowTitle("Data Ginger")
+        """
+        Create an instance of the main application's frame.
 
-        # menu
-        self.menu = self.menuBar()
-        self.setMenuBar(self.menu)
-        self.file_menu = self.menu.addMenu(self.tr("File"))
-        self.help_menu = self.menu.addMenu(self.tr("Help"))
+        Parameters
+        ----------
+        args: Arguments of QMainWindow object.
+        kwargs: Dictionary of arguments for QMainWindow object.
+        """
+        super(DataGingerWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Data Ginger")
+        self.setMenuBar(self.build_menu())
+        self.status = self.build_status_bar()
+        self.setCentralWidget(self.build_main_component())
+
+    def build_menu(self) -> QMenuBar:
+        """
+        Build the top menu.
+
+        Returns
+        -------
+            The top menu.
+        """
+        menu = self.menuBar()
+        file_menu = menu.addMenu(self.tr("File"))
+        help_menu = menu.addMenu(self.tr("Help"))
 
         quit_action = QAction(self.tr("Quit"), self)
         quit_action.setShortcut("Ctrl+Q")
         quit_action.triggered.connect(self.quit)
         quit_action.setMenuRole(QAction.NoRole)
-        self.file_menu.addAction(quit_action)
+        file_menu.addAction(quit_action)
 
         about_action = QAction(self.tr("About"), self)
         about_action.triggered.connect(self.about)
         about_action.setMenuRole(QAction.NoRole)
-        self.help_menu.addAction(about_action)
+        help_menu.addAction(about_action)
 
-        # status bar
-        self.status = self.statusBar()
-        self.status.showMessage(self.tr("Done."))
+        return menu
 
-        # add center
-        widget = QWidget()
-        layout = QVBoxLayout()
-        widget.setLayout(layout)
-        layout.addWidget(QLabel(self.tr("Coucou !")))
-        button = QPushButton(self.tr("Prout"))
-        button.clicked.connect(self.test)
-        layout.addWidget(button)
-        self.setCentralWidget(widget)
+    def build_status_bar(self) -> QStatusBar:
+        """
+        Build and initialize the status bar.
+
+        Returns
+        -------
+            The status bar.
+        """
+        status = self.statusBar()
+        status.showMessage(self.tr("Initialized."))
+
+        return status
+
+    def build_main_component(self) -> QWidget:
+        """
+        Build the main frame's component.
+
+        Returns
+        -------
+            The main component.
+        """
+        # widget = QWidget()
+        # layout = QVBoxLayout()
+        # widget.setLayout(layout)
+        # layout.addWidget(QLabel(self.tr("Coucou !")))
+        # button = QPushButton(self.tr("Prout"))
+        # button.clicked.connect(self.test)
+        # layout.addWidget(button)
+
+        widget = QTableWidget(5, 2, self)
+        widget.setHorizontalHeaderItem(1, QTableWidgetItem("Prout"))
+        widget.setVerticalHeaderItem(2, QTableWidgetItem("Prout v"))
+        widget.setItem(2, 1, QTableWidgetItem("Test"))
+
+        return widget
 
     @staticmethod
     def quit():
+        """
+        Exit the application.
+        """
         sys.exit()
 
     @staticmethod
     def about():
+        """
+        Show "about" dialog.
+        """
+        # TODO make the about dialog
         print("about")
-
-    def test(self):
-        print(self.tr("Et voilà !"))
